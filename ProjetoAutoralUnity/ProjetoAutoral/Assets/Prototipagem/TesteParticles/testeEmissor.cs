cf5607c7d8d90b3c464f88da16206c11af3752eb
Chi-ih-co <ra00297308@pucsp.edu.br>
2021-11-08 16:45:29 -0300

    Correção asset tiles + animator inimigo luz

---
+++ ProjetoAutoralUnity/ProjetoAutoral/Assets/Prototipagem/TesteParticles/testeEmissor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testeEmissor : MonoBehaviour
+{
+    private ParticleSystem partSys;
+    // Update is called once per frame
+    private void Start()
+    {
+        partSys = GetComponent<ParticleSystem>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("e")) partSys.Play(true);
+        else partSys.Stop(true);
+
+    }
+}
