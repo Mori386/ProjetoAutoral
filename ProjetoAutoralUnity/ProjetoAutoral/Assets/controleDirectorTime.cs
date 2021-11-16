0620b990578865b8a61857a2f97bbf8cd11cb3a6
Chi-ih-co <ra00297308@pucsp.edu.br>
2021-11-15 21:58:49 -0300

    Atualização animações Boss + 1ª versão timeline relógio + Hud de rádio e relógio

---
+++ ProjetoAutoralUnity/ProjetoAutoral/Assets/controleDirectorTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class controleDirectorTime : MonoBehaviour
+{
+    PlayableDirector director;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        director = GetComponent<PlayableDirector>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            director.Play();
+        }
+    }
+}
