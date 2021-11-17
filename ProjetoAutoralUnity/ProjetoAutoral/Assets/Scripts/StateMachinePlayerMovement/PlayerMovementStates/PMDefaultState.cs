021a713c67bb24c16b35feb0b5ddc5119bb06828
RMorizono <rkmorizono@gmail.com>
2021-11-17 16:36:19 -0300

    Update Player Mov Script

--- ProjetoAutoralUnity/ProjetoAutoral/Assets/Scripts/StateMachinePlayerMovement/PlayerMovementStates/PMDefaultState.cs
+++ ProjetoAutoralUnity/ProjetoAutoral/Assets/Scripts/StateMachinePlayerMovement/PlayerMovementStates/PMDefaultState.cs
@@ -16,7 +16,7 @@ public class PMDefaultState : PMBaseState
         Manager.rawInputMove.y = Input.GetAxisRaw("Vertical");// mesma coisa que o de cima so que para os botoes de mover na vertical
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            //Manager.director.Play();
+            if(Manager.director!=null)Manager.director.Play();
             Manager.TravelTime();
         }
         inventoryCount = Manager.playerInventoryManager.inventory.GetItemList().Count;
