fa7f1b9dda66834010f19a56344763dcad621a0a
Chi-ih-co <ra00297308@pucsp.edu.br>
2021-11-18 18:33:35 -0300

    Implementação Iluminação V1 + Script QG V1

---
+++ ProjetoAutoralUnity/ProjetoAutoral/Assets/Scripts/controleQG.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controleQG : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public static string puzzle1QG(int intParte)
+    {
+        switch(intParte)
+        {
+            default:
+            case 0:
+            /*teste*/case 1: return "faladoQG";
+
+        }
+    }
+}
