fec3892402b56325e2b749f7f7b06adec6ae86d9
RMorizono <rkmorizono@gmail.com>
2021-11-19 11:48:17 -0300

    SmartLayer

--- ProjetoAutoralUnity/ProjetoAutoral/Assets/Prototipagem/Rodrigo/Scripts/SmartLayer.cs
+++ ProjetoAutoralUnity/ProjetoAutoral/Assets/Prototipagem/Rodrigo/Scripts/SmartLayer.cs
@@ -6,16 +6,31 @@ public class SmartLayer : MonoBehaviour
 {
     public static Transform player { get; private set; }
     private SpriteRenderer spriteRenderer;
+    private Vector2 deltaPivotToCenter;
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(new Vector2(transform.position.x, transform.position.y) + deltaPivotToCenter, new Vector2(0.1f, 0.1f));
+    }
     private void Awake()
     {
         if (player == null) player = GameObject.FindGameObjectWithTag("Player").transform;
         spriteRenderer = GetComponent<SpriteRenderer>();
+        deltaPivotToCenter = new Vector2((spriteRenderer.bounds.size / 2).x, (spriteRenderer.bounds.size / 2).y) - spriteRenderer.sprite.pivot / spriteRenderer.sprite.pixelsPerUnit;
+        StartCoroutine(CheckLayerChange());
     }
     IEnumerator CheckLayerChange()
     {
-        if (player.position.y > transform.position.y)
+        while (true)
         {
-            sprite
+            if (player.position.y > transform.position.y + deltaPivotToCenter.y)
+            {
+                spriteRenderer.sortingOrder = 3;
+            }
+            else
+            {
+                spriteRenderer.sortingOrder = 1;
+            }
+            yield return new WaitForSeconds(0.05f);
         }
     }
 }
