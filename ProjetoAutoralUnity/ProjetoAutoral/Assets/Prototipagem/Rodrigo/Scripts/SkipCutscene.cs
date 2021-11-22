fa685ea1ff8087ebd92b6fe5cc2249ccc51aa4dd
RMorizono <rkmorizono@gmail.com>
2021-11-22 13:00:49 -0300

    Skip On Intro

--- ProjetoAutoralUnity/ProjetoAutoral/Assets/Prototipagem/Rodrigo/Scripts/SkipCutscene.cs
+++ ProjetoAutoralUnity/ProjetoAutoral/Assets/Prototipagem/Rodrigo/Scripts/SkipCutscene.cs
@@ -6,6 +6,6 @@ public class SkipCutscene : MonoBehaviour
 {
     void Update()
     {
-        if (Input.GetKeyDown(MenuConfigs.Instance.InputKeys[(int)MenuConfigs.Action.Menu])) transform.Find("LoadNextScene").gameObject.SetActive(true);
+        if (Input.GetKeyDown(MenuConfigs.Instance.InputKeys[(int)MenuConfigs.Action.Interaction])) transform.Find("LoadNextScene").gameObject.SetActive(true);
     }
 }
