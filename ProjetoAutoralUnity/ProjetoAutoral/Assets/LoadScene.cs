6ab421ff8e694daf3acf06945c36b64a39c414a3
Chi-ih-co <ra00297308@pucsp.edu.br>
2021-11-27 17:25:54 -0300

    Ajustes timelines

--- ProjetoAutoralUnity/ProjetoAutoral/Assets/LoadScene.cs
+++ ProjetoAutoralUnity/ProjetoAutoral/Assets/LoadScene.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
@@ -18,6 +19,10 @@ public class LoadScene : MonoBehaviour
     }
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<PlayableDirector>().Play();
+    }
+    public void loadScene()
     {
         SceneManager.LoadScene(nextScene);
     }
