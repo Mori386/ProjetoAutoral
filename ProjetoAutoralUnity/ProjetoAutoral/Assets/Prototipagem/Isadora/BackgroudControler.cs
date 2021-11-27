b9260e611b82b88738592a455b82e73a47973af5
Sai-Batt <isadorabattaglia@gmail.com>
2021-11-27 00:54:10 -0300

    Menu Animation-2

--- ProjetoAutoralUnity/ProjetoAutoral/Assets/Prototipagem/Isadora/BackgroudControler.cs
+++ ProjetoAutoralUnity/ProjetoAutoral/Assets/Prototipagem/Isadora/BackgroudControler.cs
@@ -9,12 +9,25 @@ public class BackgroudControler : MonoBehaviour
     [SerializeField] Animator animator;
     private IEnumerator Fade()
     {
+        float delay = 0.025f;
         while (((byte)(image.color.a * 255)) <= 230)
         {
             image.color = new Color32((byte)(image.color.r*255), (byte)(image.color.g * 255), (byte)(image.color.b * 255), (byte)((image.color.a* 255)+10));
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(delay);
         }
-
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("MenuPresenteDefaultAni"))
+        {
+            animator.SetTrigger("Pr2Ft");
+        }
+        else
+        {
+            animator.SetTrigger("Ft2Pr");
+        }
+        while (((byte)(image.color.a * 255)) >0)
+        {
+            image.color = new Color32((byte)(image.color.r * 255), (byte)(image.color.g * 255), (byte)(image.color.b * 255), (byte)((image.color.a * 255) - 10));
+            yield return new WaitForSeconds(delay);
+        }
     }
     public void StartFade()
     {
