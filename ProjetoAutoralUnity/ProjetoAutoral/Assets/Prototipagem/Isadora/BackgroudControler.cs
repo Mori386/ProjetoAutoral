cba4d1eb02e46317f0440e9d5184eb32a4bfbb66
Sai-Batt <isadorabattaglia@gmail.com>
2021-11-27 00:17:34 -0300

    Menu Animation

---
+++ ProjetoAutoralUnity/ProjetoAutoral/Assets/Prototipagem/Isadora/BackgroudControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroudControler : MonoBehaviour
+{
+    [SerializeField] Image image;
+    [SerializeField] Animator animator;
+    private IEnumerator Fade()
+    {
+        Debug.Log((image.color.a <= (230 / 255)));
+        while (image.color.a<=(230/255))
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, (image.color.a + (10 / 255)));
+            yield return new WaitForSeconds(0.1f);
+        }
+        
+    
+    }
+    public void StartFade()
+    {
+        StartCoroutine(Fade());
+    }
+
+    
+}
